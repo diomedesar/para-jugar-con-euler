fb331da7ade2dde324a3fd2858b300c7dc460b0a
Diego <diomedes.ar@gmail.com>
2014-07-30 08:43:13 -0300

    problema 1

--- para-jugar-con-euler/src/main/java/ar/diomedes/training/euler/Problema1.java
+++ para-jugar-con-euler/src/main/java/ar/diomedes/training/euler/Problema1.java
@@ -1,7 +1,28 @@
 package ar.diomedes.training.euler;
 
+import java.util.logging.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Created by diomedes on 30/07/2014.
+ *
+ * https://projecteuler.net/problem=1
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 public class Problema1 {
+    private final Log LOG = LogFactory.getLog(this.getClass());
+
+    public int solve(int top) {
+        int sum = 0;
+        for (int i = 3; i < top; i++) {
+            // LOG.info("por evaluar " + i);
+            if (i % 3 == 0 || i % 5 == 0) sum += i;
+        }
+        LOG.info("resultado = " + sum);
+        return sum;
+    }
 }
--- para-jugar-con-euler/src/test/java/ar/diomedes/training/euler/Problema1Test.java
+++ para-jugar-con-euler/src/test/java/ar/diomedes/training/euler/Problema1Test.java
@@ -1,14 +1,27 @@
 package ar.diomedes.training.euler;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by diomedes on 30/07/2014.
+ *
+ * https://projecteuler.net/problem=1
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 public class Problema1Test {
 
     @Test
-    public void XXX() {
+    public void testSumOfPrimesBelow10() {
+        Problema1 problema = new Problema1();
+        assertEquals(23, problema.solve(10));
+        assertEquals(0, problema.solve(0));
+        assertEquals(233168, problema.solve(1000));
+        assertEquals(0, problema.solve(1));
+
 
     }
 }
