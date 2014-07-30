96e8346fcefe006287f06b7efee4aa7e22a73493
Diego <diomedes.ar@gmail.com>
2014-07-30 07:02:29 -0300

    Cambios iniciales.

---
+++ para-jugar-con-euler/src/main/java/ar/diomedes/training/euler/Problema1.java
@@ -0,0 +1,7 @@
+package ar.diomedes.training.euler;
+
+/**
+ * Created by diomedes on 30/07/2014.
+ */
+public class Problema1 {
+}
---
+++ para-jugar-con-euler/src/test/java/ar/diomedes/training/euler/Problema1Test.java
@@ -0,0 +1,14 @@
+package ar.diomedes.training.euler;
+
+import org.junit.Test;
+
+/**
+ * Created by diomedes on 30/07/2014.
+ */
+public class Problema1Test {
+
+    @Test
+    public void XXX() {
+
+    }
+}
